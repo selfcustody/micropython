47014f13018e6f930c651b5ae24526284c6dd4bb
odudex <eduardo.schoenknecht@gmail.com>
2024-03-11 23:14:55 -0300

    reproducibility - stabilize build date

--- py/makeversionhdr.py
+++ py/makeversionhdr.py
@@ -71,7 +71,7 @@ def make_version_header(filename):
 #define MICROPY_GIT_TAG "%s"
 #define MICROPY_GIT_HASH "%s"
 #define MICROPY_BUILD_DATE "%s"
-""" % (git_tag, git_hash, datetime.date.today().strftime("%Y-%m-%d"))
+""" % (git_tag, git_hash, '2024-03-11')
 
     # Check if the file contents changed from last time
     write_file = True
